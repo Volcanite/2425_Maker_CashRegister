86ae98754747df902e4997015bc34a585576c925
Volcanite <matis.garbez@ensea.fr>
2026-02-12 17:41:45 +0100

    Fichier indépendant bluetooth, rfid par I2C, écran (1)

--- stm32_RFID/Caisse_enregistreuse-teste/Debug/Core/Src/RC522(pas réussi).d
+++
@@ -1,3 +0,0 @@
-Core/Src/RC522(pas réussi).o: ../Core/Src/RC522(pas\ réussi).c \
- ../Core/Inc/RC522.h
-../Core/Inc/RC522.h:
--- stm32_RFID/Caisse_enregistreuse-teste/Debug/Core/Src/pn532.d
+++
@@ -1,2 +0,0 @@
-Core/Src/pn532.o: ../Core/Src/pn532.c ../Core/Inc/pn532.h
-../Core/Inc/pn532.h:
